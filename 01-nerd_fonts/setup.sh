cce0696cc924fb7e20fa34861fb4d7df67c61c75
Sławek Piotrowski <sentinel@atteo.com>
2022-11-01 22:47:52 +0100

    Use ligatured fonts

--- 01-nerd_fonts/setup.sh
+++ 01-nerd_fonts/setup.sh
@@ -18,4 +18,9 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/NerdFontsS
 unzip -o NerdFontsSymbolsOnly.zip
 rm NerdFontsSymbolsOnly.zip
 
+rm HackLigatured-v3.003+FC3.1+JBMv2.242.zip || true
+wget https://github.com/gaplo917/Ligatured-Hack/releases/download/v3.003%2BNv2.1.0%2BFC%2BJBMv2.242/HackLigatured-v3.003+FC3.1+JBMv2.242.zip
+unzip -o HackLigatured-v3.003+FC3.1+JBMv2.242.zip
+rm HackLigatured-v3.003+FC3.1+JBMv2.242.zip
+
 fc-cache -fv
