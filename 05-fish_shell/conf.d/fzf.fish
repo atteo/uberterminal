7b1ea620eee7cac3125e82a13904c31f869b58bb
Sławek Piotrowski <sentinel@atteo.com>
2022-11-01 22:50:30 +0100

    When grepping for ViM also jump to line where search is found

--- 05-fish_shell/conf.d/fzf.fish
+++ 05-fish_shell/conf.d/fzf.fish
@@ -7,10 +7,22 @@ function fzf_ag
 
 	set -x FZF_DEFAULT_COMMAND "ag --nogroup --column --color '$token' || true"
 
-	set file_paths_selected (fzf --ansi --prompt="Ag> " --multi --bind "change:reload:ag --nogroup --column --color {q} || true" --phony --delimiter : --with-nth "1..2" --nth "2..-1" --query "$token" --preview='bat -f -H {2} {1}' --preview-window '+{2}' | cut -f1 -d':')
+	set result (fzf --ansi --prompt="Ag> " --multi --bind "change:reload:ag --nogroup --column --color {q} || true" --phony --delimiter : --with-nth "1..2" --nth "2..-1" --query "$token" --preview='bat -f -H {2} {1}' --preview-window '+{2}')
 
 	if test $status -eq 0
-		commandline --current-token --replace -- (string escape -- $file_paths_selected | string join ' ')
+		set file_paths_selected (echo "$result" | cut -f1 -d':')
+		set first_search_line (echo "$result" | cut -f2 -d':' | head -n1)
+
+		set buffer (commandline --current-buffer)
+
+		if string match -qr '^\s*vim' "$buffer"
+			commandline --current-token --replace -- "+$first_search_line $(string escape -- $file_paths_selected | string join ' ')"
+		else if [ "$buffer" = "$token" ]
+			commandline --current-buffer --replace -- "vim +$first_search_line $(string escape -- $file_paths_selected | string join ' ')"
+		else
+			commandline --current-token --replace -- (string escape -- $file_paths_selected | string join ' ')
+		end
+
 	end
 end
 
