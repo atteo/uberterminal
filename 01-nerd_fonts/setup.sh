59ea9d92c41b30bf243f7d0bbd60d9d76233d4b3
Sławek Piotrowski <sentinel@atteo.com>
2023-04-20 10:56:12 +0200

    Fix nerd-fonts installation

--- 01-nerd_fonts/setup.sh
+++ 01-nerd_fonts/setup.sh
@@ -4,6 +4,36 @@
 set -euCo pipefail
 IFS=$'\n\t'
 
+getLatestReleaseFromGitHub() {
+	local repo="$1"
+	local tagFilter="${2:-}"
+
+	local releases
+	releases="$(curl --silent "https://api.github.com/repos/$repo/releases")" || (echo >&2 "Failed to fetch releases from GitHub, try 'curl --silent \"https://api.github.com/repos/$repo/releases\"' && return 1")
+
+	local tags
+	tags="$(echo "$releases" | jq -r '.[] | .tag_name')" || (echo >&2 "Failed to get tags from GitHub API."; return 1)
+
+	if [[ -n "$tagFilter" ]]; then
+		tags="$(echo "$tags" | grep "$tagFilter")"
+	fi
+
+	local latestTag
+	latestTag="$(echo "$tags" | head -n 1)"
+
+	local result
+	result="$(curl --silent "https://api.github.com/repos/$repo/releases/tags/$latestTag" | jq -r '.assets[].browser_download_url')"
+
+	if [[ -z "$result" ]]; then
+		echo >&2 "not found"
+		exit 1
+	fi
+
+	echo -n "$result"
+}
+
+url="$(getLatestReleaseFromGitHub "ryanoasis/nerd-fonts" | grep -F "NerdFontsSymbolsOnly.zip")"
+
 set -x
 
 cp -r .config .fonts.conf.d ~/
@@ -13,9 +43,10 @@ mkdir -p ~/.fonts
 cd ~/.fonts
 
 rm NerdFontsSymbolsOnly.zip || true
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/NerdFontsSymbolsOnly.zip
+curl -L -o NerdFontsSymbolsOnly.zip "$url" 
 
 unzip -o NerdFontsSymbolsOnly.zip
+
 rm NerdFontsSymbolsOnly.zip
 
 rm HackLigatured-v3.003+FC3.1+JBMv2.242.zip || true
