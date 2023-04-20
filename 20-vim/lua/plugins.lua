196fbc80984bb99cebf794e8e4fbb7fc72302e42
Sławek Piotrowski <sentinel@atteo.com>
2023-04-20 10:58:21 +0200

    Use neo-tree

--- 20-vim/lua/plugins.lua
+++ 20-vim/lua/plugins.lua
@@ -70,22 +70,19 @@ packer.startup(function(use)
     }
 
     use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {
-            'kyazdani42/nvim-web-devicons', -- optional, for file icons
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v2.x",
+        requires = { 
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
         },
-        cmd = { 'NvimTreeToggle', 'NvimTreeOpen', 'NvimTreeClose', 'NvimTreeRefresh',
-            'NvimTreeFindFile', 'NvimTreeFindFileToggle', 'NvimTreeFocus' },
-        config = function()
-            require('nvim-tree').setup {
-                disable_netrw = true,
-                renderer = {
-                    group_empty = true
-                },
-                filters = {
-                    dotfiles = true
-                }
-            }
+        config = function ()
+            -- Unless you are still migrating, remove the deprecated commands from v1.x
+            vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+            require("neo-tree").setup({
+                close_if_last_window = false,
+            })
         end
     }
 
