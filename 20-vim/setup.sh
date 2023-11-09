b533c83e8bc27a6af7009d9c58790fbab84460a8
Sławek Piotrowski <sentinel@atteo.com>
2023-11-09 09:48:01 +0100

    Switch to LazyVim

--- 20-vim/setup.sh
+++ 20-vim/setup.sh
@@ -8,15 +8,16 @@ set -ex
 #sudo add-apt-repository -y ppa:neovim-ppa/stable
 #sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
-sudo apt-get -y install curl git aspell-pl universal-ctags build-essential cmake python3-dev golang-go fzf vifm ripgrep shfmt shellcheck ccls #nodejs #neovim 
+sudo apt-get -y install curl git aspell-pl universal-ctags build-essential cmake python3-dev golang-go fzf vifm ripgrep shfmt shellcheck ccls wl-clipboard #nodejs #neovim
 
 npm install pyright bash-language-server typescript-language-server typescript
 
 echo "Installing ViM spell checking files"
-mkdir -p ~/.vim/spell
-for lang in pl.utf-8 en.utf-8; do
-    curl -o ~/.vim/spell/$lang.spl -z ~/.vim/spell/$lang.spl "http://ftp.vim.org/pub/vim/runtime/spell/$lang.spl"
-done
+# mkdir -p ~/.vim/spell
+# for lang in pl.utf-8 en.utf-8; do
+#     curl -o ~/.vim/spell/$lang.spl -z ~/.vim/spell/$lang.spl "http://ftp.vim.org/pub/vim/runtime/spell/$lang.spl"
+# done
+#
+# ./configure.sh
 
-./configure.sh
-
+echo "Configuration disabled for now, I started using LazyVim"
