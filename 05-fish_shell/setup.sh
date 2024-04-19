8f9d9e39cbb14b1326b3adeacc3e73d4bafc779b
Sławek Piotrowski <sentinel@atteo.com>
2024-04-19 23:38:58 +0200

    Install notcurses-bin

--- 05-fish_shell/setup.sh
+++ 05-fish_shell/setup.sh
@@ -23,7 +23,7 @@ sudo apt-get update
 # Install fish shell and grc
 # sudo apt-add-repository -y ppa:fish-shell/release-2
 # sudo apt-get update
-sudo apt-get -y install fish grc ccze curl fd-find bat reptyr jq thefuck python3-pip git silversearcher-ag timg bpytop eza direnv teeldear
+sudo apt-get -y install fish grc ccze curl fd-find bat reptyr jq thefuck python3-pip git silversearcher-ag timg bpytop eza direnv teeldear notcurses-bin
 
 
 mkdir -p ~/.local/bin
