5df39601308095a6437a50a8cde28912444ab045
Sławek Piotrowski <sentinel@atteo.com>
2024-04-19 23:38:35 +0200

    Don't override ulimit

--- 05-fish_shell/conf.d/grc_config.fish
+++ 05-fish_shell/conf.d/grc_config.fish
@@ -3,7 +3,7 @@
 
 set -U grc_plugin_execs ant blkid cvs diff docker docker docker-machine env id ip iostat \
 	gcc g++ ifconfig make mount mtr netstat tail traceroute \
-       wdiff        last lsattr lsblk lspci lsmod lsof getfacl getsebool ulimit uptime nmap \
+       wdiff        last lsattr lsblk lspci lsmod lsof getfacl getsebool uptime nmap \
        fdisk findmnt free semanage sar ss sysctl systemctl stat showmount tune2fs tcpdump  \
        vmstat w who iptables ldap php pv
 
