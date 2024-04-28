67d2cadddd534b6bd31716d73faf14cab1c43e98
Sławek Piotrowski <sentinel@atteo.com>
2024-04-28 10:58:52 +0200

    procs is annoying disable it

--- 05-fish_shell/functions/ps.fish
+++ 05-fish_shell/functions/ps.fish
@@ -1,3 +1,10 @@
 function ps
-	procs --pager disable $argv
+    # if argument is -ef then remove it
+    # if test "$argv[1]" = "-ef"
+    #     set argv[1] ""
+    # end
+	# procs --pager disable --color always $argv
+    #
+    # disable for now, because 'procs --color always | less' is not working
+    /bin/ps $argv
 end
