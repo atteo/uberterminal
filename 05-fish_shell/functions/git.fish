a420e2dfac1206d99018b1cc6d1a6cfd802b5057
Sławek Piotrowski <sentinel@atteo.com>
2025-04-30 08:14:11 +0200

    Wrap git to replace --force with --force-with-lease

--- 05-fish_shell/functions/git.fish
+++ 05-fish_shell/functions/git.fish
@@ -1,24 +1,24 @@
 function git
     set -l args
     set -l replace_force 0
+    set -l is_push 0
 
     for arg in $argv
-        if test "$arg" = "push"
-            set -l is_push 1
-        end
+        switch $arg
+            case "push"
+                set is_push 1
+            case "--force"
+                set replace_force 1
+            end
     end
 
     for arg in $argv
-        if test "$arg" = "--force"
-            set replace_force 1
+        if test "$arg" = "--force" -a "$replace_force" -eq 1 -a "$is_push" -eq 1
+            echo "⚠️  Replacing '--force' with '--force-with-lease' for safer push."
+            set args $args --force-with-lease
         else
             set args $args $arg
         end
-    end
-
-    if test "$replace_force" -eq 1 -a "$is_push" = "1"
-        echo "⚠️  Replacing '--force' with '--force-with-lease' for safer push."
-        set args $args --force-with-lease
     end
 
     command git $args
